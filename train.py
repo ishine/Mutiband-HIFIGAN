2b407367302b1910fd746e2f56eea161333dd4a3
Miralan <1530013372@qq.com>
2020-11-04 20:44:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -109,8 +109,6 @@ def train(rank, a, h):
     mpd.train()
     msd.train()
 
-
-
     for epoch in range(max(0, last_epoch), a.training_epochs):
         if rank == 0:
             start = time.time()
